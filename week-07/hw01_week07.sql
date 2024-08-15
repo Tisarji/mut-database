074df7841b5e260bc93d1b3bacf4ab7cf83fd179
Jirasit <48820052+TONG2K@users.noreply.github.com>
2024-08-15 18:17:55 +0700

    :zap: Fix Update

--- week-07/hw01_week07.sql
+++ week-07/hw01_week07.sql
@@ -1,30 +1,48 @@
--- 1
-SELECT  employee.fname as "Name",
-       MAX(dependent.essn) as "Max Dependent"
-FROM    employee, dependent
-GROUP BY employee.fname;
+-- -- 1
+-- SELECT  employee.fname as "Name",
+--        MAX(dependent.essn) as "Max Dependent"
+-- FROM    employee, dependent
+-- GROUP BY employee.fname;
 
--- 2
-SELECT employee.fname AS "Name",
-       employee.salary AS "Salary",
-       COUNT(dependent.essn) AS "Count Dependent"
-  FROM employee,
-       dependent
- WHERE salary = (
-    SELECT max(employee.salary)
-      FROM employee,
-           dependent
-     WHERE employee.ssn > 2
-       AND employee.salary < 35000
-)
- GROUP BY employee.fname,
-          employee.salary,
-          dependent.essn;
+-- -- 2
+-- SELECT employee.fname AS "Name",
+--        employee.salary AS "Salary",
+--        COUNT(dependent.essn) AS "Count Dependent"
+--   FROM employee,
+--        dependent
+--  WHERE salary = (
+--     SELECT MAX(employee.salary)
+--       FROM employee,
+--            dependent
+--      WHERE employee.ssn > 2
+--        AND employee.salary < 35000
+-- )
+--  GROUP BY employee.fname,
+--           employee.salary,
+--           dependent.essn;
 
 -- 3
-SELECT count(*) AS "Count July Employee"
-  FROM employee
- WHERE TO_CHAR(
-    bdate,
-    'MM'
-) = 07;
+-- SELECT COUNT(*) as "Count July Employee"
+--   FROM employee
+--  WHERE TO_CHAR(
+--     bdate,
+--     'MM'
+-- ) = 07;
+
+-- SELECT COUNT(*) as "Count July Employee"
+--   FROM employee
+--  WHERE TO_DATE(TO_CHAR(
+--     bdate,
+--     'MM'
+-- ), 'MM') = TO_DATE('07','MM');
+
+
+-- Pactice Subject: Show Employee Name at Houton 
+-- 4
+select employee.fname as "Name",
+       dept_locations.dlocation as "Location"
+  from employee,
+       department,
+       dept_locations
+ where employee.dno = department.dnumber
+   and department.dnumber = dept_locations.dnumber;
